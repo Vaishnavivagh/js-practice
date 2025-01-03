4611e2c7ad2e8f8d3edca55a66c2d6e7dc0e8b8c
Vaishnavivagh <139456916+Vaishnavivagh@users.noreply.github.com>
2025-01-03 12:03:22 +0000

    conversion

--- .devcontainer/variable.js
+++ .devcontainer/variable.js
@@ -24,3 +24,14 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 
 console.log(typeof stringNumber);
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
