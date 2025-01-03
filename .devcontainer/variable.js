7aaadffb4857d95f559286208e53a07fd8a92622
Vaishnavivagh <139456916+Vaishnavivagh@users.noreply.github.com>
2025-01-03 05:54:58 +0000

    conversion of datatype

---
+++ .devcontainer/variable.js
@@ -0,0 +1,26 @@
+/*const accountId  =1234
+let accountEmail = "hello@gmail.com"
+var accountPassword = "1234"
+accountCity = "Surat"
+
+/*accountId = 
+
+accountEmail = "hc@gmail.com"
+accountPassword = "316"
+accountCity = "guj" 
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword, accountCity]);*/
+
+//console.log(typeof accountEmail);*/
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
